28690e518a587cf38ed3c4de8bb6da89537db18f
jaloliddinsultonov <sultonovjaloliddin07@gmail.com>
2025-01-16 17:42:38 +0100

    Second commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/aladdin/Downloads/blog_website-master/venv
+set -gx VIRTUAL_ENV /home/aladdin/Downloads/aladdin_organizer/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
